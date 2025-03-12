4800c57ffe0773e957465848a57afa9d4cf9708b
KoenDR06 <koen.de.ruiter@hotmail.com>
2025-03-12 15:50:05 +0100

    cahgnes

--- site/scripts/populateAbout.js
+++ site/scripts/populateAbout.js
@@ -1,24 +1,38 @@
 class Person {
     constructor(json) {
-        this.name = json.name;
-        this.shortName = json.name.split(" ")[0];
-        this.id = this.shortName.toLowerCase();
-        this.profilePicture = json.profile_picture;
-        this.about = json.about;
+        this.firstName = json.first_name;
+        this.lastName = json.last_name;
+        this.title = json.title ? json.title : "";
+    }
+}
+
+class Student extends Person {
+    constructor(json) {
+        super(json);
+
+        this.age = json.age;
+        this.hobbies = json.hobbies;
+        this.email = json.email;
+        this.photo = json.profile_picture;
+        this.major = json.major;
+        this.courses = json.courses;
     }
 }
 
 const aboutContainer = document.querySelector("article > section");
-const fileInput = aboutContainer.children[2]
+const fileInput = aboutContainer.children[2];
 
-fileInput.addEventListener("change", populate)
+fileInput.addEventListener("change", populate);
 
 function populate(event) {
     const file = event.target.files[0];
 
     const reader = new FileReader();
     reader.onload = () => {
-        const people = JSON.parse(reader.result).map( (person) => new Person(person) );
+        const json = JSON.parse(reader.result);
+        const people = json.students.map( (person) => new Student(person) );
+        const courses = json.courses;
+        const teachers = json.teachers.map( (teacher) => new Person(teacher) );
 
         for (let person of people) {
             /* ---- Main Content ---- */
@@ -26,12 +40,27 @@ function populate(event) {
             // Create section
             const section = document.createElement("section");
             section.classList.add("scroll-target");
-            section.id = person.id;
+            section.id = person.firstName.toLowerCase();
 
             // Create title
             const title = document.createElement("h2");
-            title.innerText = person.name;
+            title.innerText = person.title + " " + person.firstName + " " + person.lastName;
             section.appendChild(title);
+
+            // Make the entire section collapsible (https://www.w3schools.com/howto/howto_js_collapsible.asp)
+            title.addEventListener("click", function() {
+                for (let heading of document.querySelectorAll("h2")) {
+                    const content = heading.nextElementSibling;
+                    content.style.maxHeight = null;
+                }
+
+                const content = this.nextElementSibling;
+                if (content.style.maxHeight) {
+                    content.style.maxHeight = null;
+                } else {
+                    content.style.maxHeight = content.scrollHeight + "px";
+                }
+            });
 
             // Create personal text div
             const div = document.createElement("div");
@@ -41,19 +70,69 @@ function populate(event) {
             // Create profile picture in div
             const img = document.createElement("img");
             img.classList.add("profile-picture");
-            img.src = person.profilePicture;
-            img.alt = `Picture of team member ${person.shortName}`
+            img.src = person.photo;
+            img.alt = `Picture of team member ${person.firstName}`
             div.appendChild(img);
 
-            // Add text to div
-            const textDiv = document.createElement("div");
-            textDiv.classList.add("about-member-text");
-            div.appendChild(textDiv);
-            for (let p of person.about) {
-                const para = document.createElement("p");
-                para.innerHTML = p; // TODO This is technically speaking not allowed by the requirements, but how else are we supposed to make this content render right?
-                textDiv.appendChild(para);
+            // Create div for rest of the content
+            const personalInfoDiv = document.createElement("div");
+            personalInfoDiv.classList.add("about-member-text");
+            div.appendChild(personalInfoDiv);
+
+            // Age
+            const age = document.createElement("p");
+            const boldAge = document.createElement("b");
+            boldAge.innerText = "Age: ";
+            age.appendChild(boldAge);
+            age.appendChild(document.createTextNode(person.age));
+
+            // Hobbies
+            const hobbies = document.createElement("p");
+            const boldHobbies = document.createElement("b");
+            boldHobbies.innerText = "Hobbies: ";
+            hobbies.appendChild(boldHobbies);
+
+            const hobbyList = document.createElement("ul");
+            for (let hobby of person.hobbies) {
+                const li = document.createElement("li");
+                li.appendChild(document.createTextNode(hobby));
+                hobbyList.appendChild(li);
             }
+
+            // Major
+            const major = document.createElement("p");
+            const boldMajor = document.createElement("b");
+            boldMajor.innerText = "Major: ";
+            major.appendChild(boldMajor);
+            major.appendChild(document.createTextNode(person.major));
+
+            // Email
+            const email = document.createElement("p");
+            const boldEmail = document.createElement("b");
+            boldEmail.innerText = "E-mail: ";
+            email.appendChild(boldEmail);
+            email.appendChild(document.createTextNode(person.email));
+
+            // Courses
+            const courses = document.createElement("p");
+            const boldCourses = document.createElement("b");
+            boldCourses.innerText = "Courses: ";
+            courses.appendChild(boldCourses);
+
+            const courseList = document.createElement("ul");
+            for (let course of person.courses) {
+                const li = document.createElement("li");
+                li.appendChild(document.createTextNode(course));
+                courseList.appendChild(li);
+            }
+
+            personalInfoDiv.appendChild(age);
+            personalInfoDiv.appendChild(major);
+            personalInfoDiv.appendChild(email);
+            personalInfoDiv.appendChild(hobbies);
+            personalInfoDiv.appendChild(hobbyList);
+            personalInfoDiv.appendChild(courses);
+            personalInfoDiv.appendChild(courseList)
 
             aboutContainer.appendChild(section);
         }
@@ -61,6 +140,6 @@ function populate(event) {
 
     reader.readAsText(file);
 
-    fileInput.remove()
-    aboutContainer.children[1].remove()
+    fileInput.remove();
+    aboutContainer.children[1].remove();
 }
