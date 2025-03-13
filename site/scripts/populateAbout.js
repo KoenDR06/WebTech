4d0fe8e3f1872415dabab92ae3916da4f49a3f09
KoenDR06 <koen.de.ruiter@hotmail.com>
2025-03-13 15:12:45 +0100

    Added quotes to about page and finalized setters for Student and Person

--- site/scripts/populateAbout.js
+++ site/scripts/populateAbout.js
@@ -4,9 +4,9 @@ class Person {
     #title;
 
     constructor(json) {
-        this.#firstName = json.first_name;
-        this.#lastName = json.last_name;
-        this.#title = json.title ? json.title : "";
+        this.firstName = json.first_name;
+        this.lastName = json.last_name;
+        this.title = json.title ? json.title : "";
     }
 
     get firstName() {return this.#firstName}
@@ -26,8 +26,7 @@ class Person {
     }
 
     set title(value) {
-        if (!value instanceof String ||
-            value === "") return;
+        if (!value instanceof String) return;
         this.#title = value;
     }
 }
@@ -44,13 +43,13 @@ class Student extends Person {
     constructor(json) {
         super(json);
 
-        this.#age = json.age;
-        this.#email = json.email;
-        this.#hobbies = json.hobbies;
-        this.#photo = json.profile_picture;
-        this.#major = json.major;
-        this.#courses = json.courses;
-        this.#quote = json.quote;
+        this.age = json.age;
+        this.email = json.email;
+        this.hobbies = json.hobbies;
+        this.photo = json.profile_picture;
+        this.major = json.major;
+        this.courses = json.courses;
+        this.quote = json.quote;
     }
 
     get age() {return this.#age}
@@ -62,14 +61,14 @@ class Student extends Person {
     get quote() {return this.#quote}
 
     set age(value) {
-        if (!Number.isInteger(value) || //Check of goed is.
+        if (!Number.isInteger(value) ||
             value < 0 || value > 90 ) return;
         this.#age = value;
     }
 
     set email(value) {
-        if (!value instanceof String || //Done
-            value === "") return;
+        // Source: https://regex101.com/r/SOgUIV/2
+        if (!/^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/.test(value)) return;
         this.#email = value;
     }
 
@@ -115,21 +114,21 @@ function populate(event) {
     const reader = new FileReader();
     reader.onload = () => {
         const json = JSON.parse(reader.result);
-        const people = json.students.map( (person) => new Student(person) );
+        const students = json.students.map( (person) => new Student(person) );
         const courses = json.courses;
         const teachers = json.teachers.map( (teacher) => new Person(teacher) );
 
-        for (let person of people) {
+        for (let student of students) {
             /* ---- Main Content ---- */
 
             // Create section
             const section = document.createElement("section");
             section.classList.add("scroll-target");
-            section.id = person.firstName.toLowerCase();
+            section.id = student.firstName.toLowerCase();
 
             // Create title
             const title = document.createElement("h2");
-            title.innerText = person.title + " " + person.firstName + " " + person.lastName;
+            title.innerText = student.title + " " + student.firstName + " " + student.lastName;
             section.appendChild(title);
 
             // Make the entire section collapsible (https://www.w3schools.com/howto/howto_js_collapsible.asp)
@@ -148,13 +147,25 @@ function populate(event) {
             div.classList.add("team-member");
             section.appendChild(div);
 
-            // Create profile picture in div
-            const img = document.createElement("img");
+            // Create div for image and quote
+            const imageDiv = document.createElement("div");
 
+            // Create profile picture in image div
+            const img = document.createElement("img");
             img.classList.add("team-member__picture");
-            img.src = person.photo;
-            img.alt = `Picture of team member ${person.firstName}`
-            div.appendChild(img);
+            img.src = student.photo;
+            img.alt = `Picture of team member ${student.firstName}`
+            imageDiv.appendChild(img);
+
+            // Create quote paragraph under image
+            const quote = document.createElement("p");
+            quote.classList.add("team-member__quote")
+            const italic = document.createElement("em");
+            quote.appendChild(italic);
+            italic.innerText = `"${student.quote}"`
+            imageDiv.appendChild(quote);
+
+            div.appendChild(imageDiv);
 
             // Create div for rest of the content
             const personalInfoDiv = document.createElement("div");
@@ -166,7 +177,7 @@ function populate(event) {
             const boldAge = document.createElement("b");
             boldAge.innerText = "Age: ";
             age.appendChild(boldAge);
-            age.appendChild(document.createTextNode(person.age));
+            age.appendChild(document.createTextNode(student.age));
 
             // Hobbies
             const hobbies = document.createElement("p");
@@ -175,7 +186,7 @@ function populate(event) {
             hobbies.appendChild(boldHobbies);
 
             const hobbyList = document.createElement("ul");
-            for (let hobby of person.hobbies) {
+            for (let hobby of student.hobbies) {
                 const li = document.createElement("li");
                 li.appendChild(document.createTextNode(hobby));
                 hobbyList.appendChild(li);
@@ -186,14 +197,14 @@ function populate(event) {
             const boldMajor = document.createElement("b");
             boldMajor.innerText = "Major: ";
             major.appendChild(boldMajor);
-            major.appendChild(document.createTextNode(person.major));
+            major.appendChild(document.createTextNode(student.major));
 
             // Email
             const email = document.createElement("p");
             const boldEmail = document.createElement("b");
             boldEmail.innerText = "E-mail: ";
             email.appendChild(boldEmail);
-            email.appendChild(document.createTextNode(person.email));
+            email.appendChild(document.createTextNode(student.email));
 
             // Courses
             const courses = document.createElement("p");
@@ -202,7 +213,7 @@ function populate(event) {
             courses.appendChild(boldCourses);
 
             const courseList = document.createElement("ul");
-            for (let course of person.courses) {
+            for (let course of student.courses) {
                 const li = document.createElement("li");
                 li.appendChild(document.createTextNode(course));
                 courseList.appendChild(li);
