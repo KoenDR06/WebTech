9083feb28722428d7ad7914dab715ba057450afd
KoenDR06 <koen.de.ruiter@hotmail.com>
2025-03-13 14:43:34 +0100

    Cleaned up collapsing logic

--- site/scripts/populateAbout.js
+++ site/scripts/populateAbout.js
@@ -140,11 +140,7 @@ function populate(event) {
                 }
 
                 const content = this.nextElementSibling;
-                if (content.style.maxHeight) {
-                    content.style.maxHeight = null;
-                } else {
-                    content.style.maxHeight = content.scrollHeight + "px";
-                }
+                content.style.maxHeight = content.scrollHeight + "px";
             });
 
             // Create personal text div
