00a37dec04bcaa2bdbceee5bd4298fd3305cb5ce
KoenDR06 <koen.de.ruiter@hotmail.com>
2025-03-19 14:57:32 +0100

    Added db and populated it with tables but no data yet

---
+++ populate-db.sql
@@ -0,0 +1,72 @@
+CREATE TABLE "Courses"
+(
+    id       integer not null
+        constraint id
+            primary key autoincrement,
+    major_id integer not null,
+    name     text    not null
+)
+
+CREATE TABLE "Friends"
+(
+    user_a_id integer not null
+        constraint Friend_A
+            references Users,
+    user_b_id integer not null
+        constraint Friend_B
+            references Users,
+    accepted  integer not null,
+    id        integer not null
+        constraint id
+            primary key autoincrement,
+    constraint users
+        unique (user_a_id, user_b_id)
+)
+
+CREATE TABLE Majors
+(
+    id   integer not null
+        constraint id
+            primary key autoincrement,
+    name text    not null
+)
+
+CREATE TABLE Messages
+(
+    id        integer not null
+        constraint id
+            primary key autoincrement,
+    content   text    not null,
+    timestamp integer not null,
+    sender_id integer not null
+        constraint sender_id
+            references Users,
+    friend_id integer not null
+        constraint friend_id
+            references Friends (id)
+)
+
+CREATE TABLE sqlite_sequence(name,seq)
+
+CREATE TABLE User_Courses
+(
+    user_id   integer not null,
+    course_id integer not null,
+    constraint id
+        primary key (user_id, course_id)
+)
+
+CREATE TABLE "Users"
+(
+    id         integer not null
+        constraint id
+            primary key autoincrement,
+    first_name text    not null,
+    last_name  text    not null,
+    birth_date integer not null,
+    email      text    not null,
+    major_id   integer not null
+        constraint major_id
+            references Majors,
+    hobbies    text    not null
+    , photo_path text not null)
